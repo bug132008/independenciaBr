cecdf69e1039e9eb6ec941a0b4642c4b2e74843d
bug132008 <nhaneye@gmail.com>
2022-08-31 21:24:19 -0300

    first

---
+++ main.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+	return render_template('index.html')
+@app.route('/<any>')
+def ani(any):
+	return f"A página {any} não foi encontrada"
+if __name__ == "__main__":
+	app.run(debug=True, port=8080)
